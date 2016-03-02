2c71813a4e37e7b40e57cef83ca5282cfed79906
Morgan Cheng <morgan.cheng@hulu.com>
2016-03-02 19:06:34 +0800

    init

---
+++ my/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+My::Application.config.secret_token = '092f0baaf36165ad387c83f9f094057040cc7d9873dbf63e673b8ca6c5410fcf8d858e6a6212269c858e89bb029aa5d218d94b2aeef95248a37dedefd05f9d20'
