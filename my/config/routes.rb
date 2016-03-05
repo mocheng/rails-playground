8e2582a6cf65af9db6476c677ef0f8fc5f0c6c13
Morgan Cheng <morgan.chengmo@gmail.com>
2016-03-05 23:53:58 +0800

    add static_page

--- my/config/routes.rb
+++ my/config/routes.rb
@@ -1,4 +1,6 @@
 My::Application.routes.draw do
+  get "static_page/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ My::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'static_page#home'
 
   # See how all your routes lay out with "rake routes"
 
