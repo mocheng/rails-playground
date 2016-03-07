20a018567f05bcca2b498ad5a029a89bedeeedf0
Morgan Cheng <morgan.cheng@hulu.com>
2016-03-07 17:22:12 +0800

    add count test code

--- my/app/controllers/static_page_controller.rb
+++ my/app/controllers/static_page_controller.rb
@@ -1,4 +1,18 @@
 class StaticPageController < ApplicationController
+  layout 'application'
+
+  @@visit_count = 0
+
+  def initialize
+    @count = 0
+    puts 'new class instance'
+  end
+
   def home
+#    @count += 1
+#    @@visit_count += 1
+
+#    puts @count
+#    puts @@visit_count
   end
 end
